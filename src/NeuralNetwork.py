c1abe103e4d48ecc86670fdf58c20bc2ee878ea6
Pierre Gerard <piegerar@ulb.ac.be>
2015-12-03 11:20:15 -0500

    [fix] minor

--- src/NeuralNetwork.py
+++ src/NeuralNetwork.py
@@ -30,7 +30,6 @@ class NeuralNetwork(object):
         self._gradb2 = np.array([[i] for i in self._gradoa])
         self._gradw2 = np.dot(np.array([[i] for i in self._gradoa]), np.transpose(self._hs))
         self._gradhs = np.dot(np.transpose(self._w2), np.array([[i] for i in self._gradoa]))
-        self._gradha = [[self._gradhs[j][i] if (self._ha[j][i] > 0) else 0 for i in range(len(self._gradhs[j]))] for j in range(len(self._gradhs))]
         self._gradha = self._gradhs * np.where(self._ha > 0, 1, 0)
         self._gradb1 = np.array([[i] for i in self._gradha])
         self._gradw1 = np.dot(self._gradha, np.transpose(X))
@@ -38,96 +37,6 @@ class NeuralNetwork(object):
 
     def minibatch(self):
         pass
-
-def verifGradient():
-    sigma = 1e-4
-
-    neuralNetwork = NeuralNetwork(3,4,2)
-    X = [0.4,0.7,0.3]
-    y = 1 # imaginons que c'est un point de la classe 2
-    neuralNetwork.fprop(X)
-    neuralNetwork.bprop(X,y)
-    calculategrad = neuralNetwork._gradw1
-    lostInitial = -(np.log(neuralNetwork._os[y][0]))
-
-    numgrad = []
-    for i in range(len(neuralNetwork._w1)):
-        for j in range(len(neuralNetwork._w1[0])):
-            neuralNetwork._w1[i][j] += sigma
-            neuralNetwork.fprop(X)
-            neuralNetwork.bprop(X,y)
-            lost2 = -(np.log(neuralNetwork._os[y][0]))
-            neuralNetwork._w1[i][j] -= sigma
-            numgrad.append((lost2 - lostInitial) / sigma)
-
-    print("Verification gradient w1")
-    print(utils.ratioGrad(list(calculategrad.ravel()), numgrad))
-
-    neuralNetwork = NeuralNetwork(3,4,2)
-    X = [0.4,0.7,0.3]
-    y = 1 # imaginons que c'est un point de la classe 2
-    neuralNetwork.fprop(X)
-    neuralNetwork.bprop(X,y)
-    calculategrad = neuralNetwork._gradb1
-    lostInitial = -(np.log(neuralNetwork._os[y][0]))
-
-    numgrad = []
-    for i in range(len(neuralNetwork._b1)):
-        for j in range(len(neuralNetwork._b1[0])):
-            neuralNetwork._b1[i][j] += sigma
-            neuralNetwork.fprop(X)
-            neuralNetwork.bprop(X,y)
-            lost2 = -(np.log(neuralNetwork._os[y][0]))
-            neuralNetwork._b1[i][j] -= sigma
-            numgrad.append((lost2 - lostInitial) * 1./ sigma)
-
-    print("Verification gradient b1")
-    print(utils.ratioGrad(list(calculategrad.ravel()), numgrad))
-
-    neuralNetwork = NeuralNetwork(3,4,2)
-    X = [0.6,0.7,0.3]
-    y = 1 # imaginons que c'est un point de la classe 2
-    neuralNetwork.fprop(X)
-    neuralNetwork.bprop(X,y)
-    calculategrad = neuralNetwork._gradw2
-    lostInitial = -(np.log(neuralNetwork._os[y][0]))
-
-    numgrad = []
-    for i in range(len(neuralNetwork._w2)):
-        for j in range(len(neuralNetwork._w2[0])):
-            neuralNetwork._w2[i][j] += sigma
-            neuralNetwork.fprop(X)
-            neuralNetwork.bprop(X,y)
-            lost2 = -(np.log(neuralNetwork._os[y][0]))
-            neuralNetwork._w2[i][j] -= sigma
-            numgrad.append((lost2 - lostInitial) * 1./ sigma)
-
-    print("Verification gradient w2")
-    print(utils.ratioGrad(list(calculategrad.ravel()), numgrad))
-
-
-    neuralNetwork = NeuralNetwork(3,4,2)
-    X = [0.4,0.7,0.3]
-    y = 1 # imaginons que c'est un point de la classe 2
-    neuralNetwork.fprop(X)
-    neuralNetwork.bprop(X,y)
-    calculategrad = neuralNetwork._gradb2
-    lostInitial = -(np.log(neuralNetwork._os[y][0]))
-
-    numgrad = []
-    for i in range(len(neuralNetwork._b2)):
-        for j in range(len(neuralNetwork._b2[0])):
-            neuralNetwork._b2[i][j] += sigma
-            neuralNetwork.fprop(X)
-            neuralNetwork.bprop(X,y)
-            lost2 = -(np.log(neuralNetwork._os[y][0]))
-            neuralNetwork._b2[i][j] -= sigma
-            numgrad.append((lost2 - lostInitial) * 1./ sigma)
-
-    print("Verification gradient b2")
-    print(utils.ratioGrad(list(calculategrad.ravel()), numgrad))
-
-
 
 
 if __name__ == '__main__':
