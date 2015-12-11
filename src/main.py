b8add0afff5c28ea8db6225b43d5e298d0aa0513
Pierre Gerard <piegerar@ulb.ac.be>
2015-12-11 00:44:02 -0500

    [add] tweak to iter a high number of times

--- src/main.py
+++ src/main.py
@@ -39,41 +39,31 @@ def main():
     print("\n\n>>EXERCICE 5 Entrainement du reseau de neuronne + Variation des hyper-parametres")
     X, y = readMoonFile()
 
-    default_h = 5
-    sample_h = [2,5,10,100,1000]
+    #default_h = 5
+    sample_h = [2,5,10,50,100,200,500,1000]
 
-    default_wd = 0.1
+    #wd = 0.1
     sample_wd = [0,0.00001,0.0001,0.001,0.01]  #todo Valider terme de regularisation dans NeuralNetwork. Lorsque != 0, validations #1,2,3,4 ne sont plus bon...
 
     default_maxIter = 5
-    sample_maxIter = [1,2,5,10,20]
+    sample_maxIter = [1,2,5,10,20,100]
 
-    for h in sample_h:
-        neuralNetwork = trainNetwork(X, y,  default_wd, h, default_maxIter)
-        predictions = neuralNetwork.computePredictions(X)
+    i = 0
+    while True
+        for h in sample_h:
+            for wd in sample_wd:
+                for maxIter in sample_maxIter:
+                    i+=1
+                    neuralNetwork = trainNetwork(X, y,  wd, h, maxIter)
+                    predictions = neuralNetwork.computePredictions(X)
 
-        trainEfficiency = calculatePredictionsEfficiency(predictions, y)
-        title = "h: "+str(h)+" / wd: "+str(default_wd)+" / "+str(default_maxIter)+" epoques"+" / Train Err: "+str(100-trainEfficiency)+"%"
-        name = "regions_decision"+str(h)+"_"+str(default_wd)+"_"+str(default_maxIter)
-        plotRegionsDescision(X, y, neuralNetwork, title, name)
-
-    for wd in sample_wd:
-        neuralNetwork = trainNetwork(X, y,  wd, default_h, default_maxIter)
-        predictions = neuralNetwork.computePredictions(X)
-
-        trainEfficiency = calculatePredictionsEfficiency(predictions, y)
-        title = "h: "+str(default_h)+" / wd: "+str(wd)+" / "+str(default_maxIter)+" epoques"+" / Train Err: "+str(100-trainEfficiency)+"%"
-        name = "regions_decision"+str(default_h)+"_"+str(wd)+"_"+str(default_maxIter)
-        plotRegionsDescision(X, y, neuralNetwork, title, name)
-
-    for maxIter in sample_maxIter:
-        neuralNetwork = trainNetwork(X, y,  default_wd, default_h, maxIter)
-        predictions = neuralNetwork.computePredictions(X)
-
-        trainEfficiency = calculatePredictionsEfficiency(predictions, y)
-        title = "h: "+str(default_h)+" / wd: "+str(default_wd)+" / "+str(maxIter)+" epoques"+" / Train Err: "+str(100-trainEfficiency)+"%"
-        name = "regions_decision"+str(default_h)+"_"+str(default_wd)+"_"+str(maxIter)
-        plotRegionsDescision(X, y, neuralNetwork, title, name)
+                    trainEfficiency = calculatePredictionsEfficiency(predictions, y)
+                    if trainEfficiency > 80:
+                        print("h: "+str(h)+" / wd: "+str(wd)+" / "+str(maxIter)+" epoques"+" / Train Err: "+str(100-trainEfficiency)+"%")
+                    if trainEfficiency > 99:
+                        title = "h: "+str(h)+" / wd: "+str(wd)+" / "+str(maxIter)+" epoques"+" / Train Err: "+str(100-trainEfficiency)+"%"
+                        name = str(100-trainEfficiency) + "_regions_decision"+str(h)+"_"+str(wd)+"_"+str(maxIter) + "_" + str(i)
+                        plotRegionsDescision(X, y, neuralNetwork, title, name)
 
 if __name__ == '__main__':
     main()
